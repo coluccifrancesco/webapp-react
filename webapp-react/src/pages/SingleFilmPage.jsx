47f071f6ee49ee68aa6214a0db059093a47368fa
coluccifrancesco <coluccif2002@gmail.com>
2025-08-07 17:11:21 +0200

    Implemented logic for POST request

--- webapp-react/src/pages/SingleFilmPage.jsx
+++ webapp-react/src/pages/SingleFilmPage.jsx
@@ -9,16 +9,51 @@ export default function HomeAndFilmList() {
     const { id } = useParams();
     const reviews = movie.reviews;
 
+    // Form
+    const [ formData, setFormData ] = useState({
+        userName: '',
+        userReview: '',
+        userVote: ''
+    });
+
+    function handleFormData(e){
+
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;
+
+        setFormData(prevData => ({
+            ...prevData, [fieldName]: fieldValue
+        }))
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log('Review Sent!', formData);
+        
+        fetch(`http://localhost:3070/api/movies/${id}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: formData.userName,
+                text: formData.userReview,
+                vote: formData.userVote
+            })
+        })
+
+        .then(()=>fetchFilm())
+    }
+
+
     if (id) {
-        console.log(id);
+        console.log('Film id: ' + id);
     }
 
     function fetchFilm() {
         return fetch(`http://localhost:3070/api/movies/${id}`)
             .then(res => res.json())
             .then(data => {
-                setMovie(data)
-            })
+            setMovie(data)
+        })
     }
 
     useEffect(() => { fetchFilm() }, []);
@@ -68,23 +103,48 @@ export default function HomeAndFilmList() {
         <section className='form-review'>
             <h2>Share your opinion on the film!</h2>
 
-            <form className='container'>
+            <form 
+                className='container'
+                onSubmit={handleSubmit}
+            >
                 <div className="mb-3">
-                    <label htmlFor="name-input" className="form-label">Name</label>
-                    <input type="email" className="form-control" id="name-input" aria-describedby="emailHelp" />
+                    <label htmlFor="input-name" className="form-label">Name</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        name="userName"  
+                        id="input-name"  
+                        value={formData.userName} 
+                        onChange={handleFormData}
+                    />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="input-review" className="form-label">Your review</label>
-                    <input type="text" className="form-control" id="input-review" />
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        name="userReview" 
+                        id="input-review" 
+                        value={formData.userReview} 
+                        onChange={handleFormData}
+                    />
                 </div>
                 <div className="mb-3">
                     
-                    <label className="form-label" htmlFor="vote-input">Your vote</label>
+                    <label className="form-label" htmlFor="input-vote">Your vote</label>
                     
                     <div className='d-flex justify-content-between align-items-center'>
 
                         <div>
-                            <input type="number" className="vote-input form-control" id="vote-input" min="1" max="5" />
+                            <input 
+                                type="number" 
+                                className="vote-input form-control" 
+                                name="userVote" 
+                                id="input-vote" 
+                                min="1" max="5" 
+                                value={formData.userVote}
+                                onChange={handleFormData} 
+                            />
                         </div>
                     
                         <button type="submit" className="submit-btn">Submit</button>
